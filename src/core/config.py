a2673b5b92a85787aff0d5c03d6cb2d42b1e5ee4
Átila <atilalimade@gmail.com>
2026-04-21 13:17:30 -0300

    chore: adicionando configs, atualizando dependencias

--- src/core/config.py
+++ src/core/config.py
@@ -9,7 +9,8 @@ load_dotenv('.env')
 PIPEFY_TOKEN = getenv("PIPEFY_TOKEN")
 PIPE_ID = getenv("PIPE_ID", "303843596")
 
-DB_URL = getenv("DB_URL", "sqlite:///./db.sqlite3")
+DB_URL = getenv("DB_URL", "sqlite:///./db.sqlite")
+IS_CREATE_DEMO_API_STARTUP = getenv("CREATE_DEMO_API_STARTUP", "true").lower() == "true"
 
 PROJECT_TIMEZONE = getenv("PROJECT_TIMEZONE", "America/Fortaleza")
 PROJECT_TZ = ZoneInfo(PROJECT_TIMEZONE)
@@ -18,3 +19,9 @@ PIPEFY_GRAPHQL_ENDPOINT = "https://api.pipefy.com/graphql"
 PIPEFY_GRAPHQL_TIMEOUT_SECONDS = float(
 	getenv("PIPEFY_GRAPHQL_TIMEOUT_SECONDS", "30"),
 )
+
+API_BOOTSTRAP_PERSON_NAME = getenv("API_BOOTSTRAP_PERSON_NAME", "demo-api-person")
+
+PIPEFY_FIELD_ID_NAME = getenv("PIPEFY_FIELD_ID_NAME", "name")
+PIPEFY_FIELD_ID_EMAIL = getenv("PIPEFY_FIELD_ID_EMAIL", "email")
+PIPEFY_FIELD_ID_TAX_ID = getenv("PIPEFY_FIELD_ID_TAX_ID", "tax_id")
