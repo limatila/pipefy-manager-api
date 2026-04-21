35a2c985387ce8d18058e6b84c331052715c9176
Átila <atilalimade@gmail.com>
2026-04-21 10:45:28 -0300

    refator: mudando nome de módulo

--- src/clients/__init__.py
+++
@@ -1,9 +0,0 @@
-from clients.pipefy_graphql_client import PipefyGraphQLClient
-from clients.pipefy_operation_builder import PipefyOperationBuilder
-from clients.pipefy_response_parser import PipefyResponseParser
-
-__all__ = [
-	"PipefyGraphQLClient",
-	"PipefyOperationBuilder",
-	"PipefyResponseParser",
-]
---
+++ src/pipefy_clients/__init__.py
@@ -0,0 +1,9 @@
+from pipefy_clients.pipefy_graphql_client import PipefyGraphQLClient
+from pipefy_clients.pipefy_operation_builder import PipefyOperationBuilder
+from pipefy_clients.pipefy_response_parser import PipefyResponseParser
+
+__all__ = [
+	"PipefyGraphQLClient",
+	"PipefyOperationBuilder",
+	"PipefyResponseParser",
+]
